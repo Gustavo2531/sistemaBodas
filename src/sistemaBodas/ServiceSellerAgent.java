5e5adcc7a2faa4231d5379f990b042a85ff1f0c2
Gustavo3125 <gustavo31.25@gmail.com>
2017-10-22 23:13:10 -0500

    Proyecto Sistemas Multiagentes

---
+++ src/sistemaBodas/ServiceSellerAgent.java
@@ -0,0 +1,132 @@
+package sistemaBodas;
+
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
+import java.util.*;
+
+public class ServiceSellerAgent extends Agent {
+
+	private Hashtable catalogue;
+	
+	private ServiceSellerGui myGui;
+
+	// Put agent initializations here
+	protected void setup() {
+		// Create the catalogue
+		catalogue = new Hashtable();
+
+		// Create and show the GUI 
+		myGui = new ServiceSellerGui(this);
+		myGui.showGui();
+
+		
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("service-selling");
+		sd.setName("JADE-wedding-service-trading");
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		}
+		catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+
+		// Add the behaviour serving queries from buyer agents
+		addBehaviour(new OfferRequestsServer());
+
+		// Add the behaviour serving purchase orders from buyer agents
+		addBehaviour(new PurchaseOrdersServer());
+	}
+
+	// Put agent clean-up operations here
+	protected void takeDown() {
+		// Deregister from the yellow pages
+		try {
+			DFService.deregister(this);
+		}
+		catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+		// Close the GUI
+		myGui.dispose();
+		// Printout a dismissal message
+		System.out.println("Seller-agent "+getAID().getName()+" terminating.");
+	}
+
+
+	
+	public void updateCatalogue(final String title, final int price) {
+		addBehaviour(new OneShotBehaviour() {
+			public void action() {
+				catalogue.put(title, new Integer(price));
+				System.out.println(title+" inserted into catalogue. Price = "+price);
+			}
+		} );
+	}
+
+	
+	private class OfferRequestsServer extends CyclicBehaviour {
+		public void action() {
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+			ACLMessage msg = myAgent.receive(mt);
+			if (msg != null) {
+				// CFP Message received. Process it
+				String title = msg.getContent();
+				ACLMessage reply = msg.createReply();
+
+				Integer price = (Integer) catalogue.get(title);
+				if (price != null) {
+					
+					reply.setPerformative(ACLMessage.PROPOSE);
+					reply.setContent(String.valueOf(price.intValue()));
+				}
+				else {
+					
+					reply.setPerformative(ACLMessage.REFUSE);
+					reply.setContent("not-available");
+				}
+				myAgent.send(reply);
+			}
+			else {
+				block();
+			}
+		}
+	}  // End of inner class OfferRequestsServer
+
+
+	private class PurchaseOrdersServer extends CyclicBehaviour {
+		public void action() {
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+			ACLMessage msg = myAgent.receive(mt);
+			if (msg != null) {
+				// ACCEPT_PROPOSAL Message received. Process it
+				String title = msg.getContent();
+				ACLMessage reply = msg.createReply();
+
+				Integer price = (Integer) catalogue.remove(title);
+				if (price != null) {
+					reply.setPerformative(ACLMessage.INFORM);
+					System.out.println(title+" sold to agent "+msg.getSender().getName());
+				}
+				else {
+					
+					reply.setPerformative(ACLMessage.FAILURE);
+					reply.setContent("not-available");
+				}
+				myAgent.send(reply);
+			}
+			else {
+				block();
+			}
+		}
+	}  // End of inner class OfferRequestsServer
+}
